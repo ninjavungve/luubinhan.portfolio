01d1fd550dd4fc2b8c351265b8ee24cfb813b282
An Luu <an.luu@creatory.vn>
2018-09-26 18:21:05 +0700

    +

--- src/html.js
+++ src/html.js
@@ -10,6 +10,9 @@ export default class HTML extends React.Component {
           <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
           <meta name="format-detection" content="telephone=no" />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossOrigin="anonymous" />
+          <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css" />
+          <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet" />
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,19 +2,86 @@ import React from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 import Layout from "../layout";
-import PostListing from "../components/PostListing/PostListing";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 
 class Index extends React.Component {
   render() {
-    const postEdges = this.props.data.allMarkdownRemark.edges;
     return (
       <Layout>
         <div className="index-container">
           <Helmet title={config.siteTitle} />
           <SEO />
-          <PostListing postEdges={postEdges} />
+          <div className='master'>
+            <div className="master-inner">
+              <section className="section section-portfolio">
+                <div className="inner">
+                  <div className="container">
+                    <div className="section-header">
+                      <h1>Home</h1>
+                    </div>
+                    <div className="section-body">
+                      <div className="row">
+                        <div className="col-sm">
+                          <div className="card-portfolio">
+                            <div className="card-image">
+                            <img src="https://vip-restaurant.vamtam.com/wp-content/uploads/2016/03/Screen-1.jpg" alt=""/>
+                            </div>
+                            <div className="card-title">
+                              Gourmet / Restaurant
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-sm">
+                          <div className="card-portfolio">
+                            <div className="card-image">
+                              <img src="https://vip-restaurant.vamtam.com/wp-content/uploads/2016/03/Screen-2.jpg"/>
+                            </div>
+                            <div className="card-title">
+                              Gourmet / Restaurant
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-sm">
+                          <div className="card-portfolio">
+                            <div className="card-image">
+                              <img src="https://vip-restaurant.vamtam.com/wp-content/uploads/2016/03/Screen-3.jpg" />
+                            </div>
+                            <div className="card-title">
+                              Coffee Shop
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+        <div className="scroll-top-wrapper">
+          <a className="scroll_top" href="#top">
+            <i className="ion-android-arrow-dropup-circle"></i>
+          </a>  
+        </div>  
+        <div id="footer">
+          <footer className="footer">
+            <section className="footer-inner">
+              <div className="container">
+                <div className="row">
+                  <div className="col">
+                    <small>
+                    Email: <a href="mailto:luubinhan1989@gmail.com">luubinhan1989@gmail.com</a>
+                    </small>
+                  </div>
+                  <div className="col text-right">
+                    <small>© Copyright 2018 luubinhan, All rights reserved</small>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </footer>
         </div>
       </Layout>
     );
